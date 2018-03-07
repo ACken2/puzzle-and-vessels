82bd89f76b99b8fdf74d1a17ef22558ee55328ad
Ken Sze <acken2@outlook.com>
2018-03-07 22:39:17 +0900

    Implemented a basic game panel

---
+++ Assets/Scripts/Orbs/Orb.cs
@@ -0,0 +1,132 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using System;
+using System.Collections;
+
+namespace Assets.Scripts.Orbs {
+
+    public class Orb : MonoBehaviour {
+
+        // Publicly set variable
+        public int row;
+        public int column;
+        public Sprite typeOneOrb;
+        public Sprite typeTwoOrb;
+        public Sprite typeThreeOrb;
+
+        private int type = 1;
+
+        // 
+        private bool mouseDown = false;
+        private const float selectedAlpha = 0.8f;
+
+        // Used in orb swapping movement
+        private int animationConstant = 10;
+        private int counter = 0;
+        private Vector3 moveTarget = Vector3.zero;
+        private Vector3 moveSpeed = Vector3.zero;
+        private Vector3 _preRotateionPos = Vector3.zero;
+
+        private static readonly System.Random getrandom = new System.Random();
+
+        // Use this for initialization
+        public void Start() {
+            // Setup listener to BeginDrag, Drag and EndDrag
+            EventTrigger trigger = GetComponent<EventTrigger>();
+            EventTrigger.Entry dragEntry = new EventTrigger.Entry();
+            dragEntry.eventID = EventTriggerType.Drag;
+            dragEntry.callback.AddListener((data) => { OnDragDelegate((PointerEventData)data); });
+            EventTrigger.Entry endDragEntry = new EventTrigger.Entry();
+            endDragEntry.eventID = EventTriggerType.EndDrag;
+            endDragEntry.callback.AddListener((data) => { OnEndDragDelegate((PointerEventData)data); });
+            trigger.triggers.Add(dragEntry);
+            trigger.triggers.Add(endDragEntry);
+            // Register to OrbPanel
+            OrbPanel.regOrb(this, row, column);
+            // Randomize orb type
+            type = GetRandomNumber(1, 4);
+            GetComponent<SpriteRenderer>().sprite = getCurrentSprite();
+        }
+
+        // Update is called once per frame
+        public void Update() {
+            if (moveTarget.magnitude != 0) {
+                // rotate();
+                move();
+            }
+        }
+
+        public int getType() {
+            return type;
+        }
+
+        public void OnMouseDown() {
+            OrbPanel.OnBeginDrag(this, row, column);
+            mouseDown = true;
+        }
+
+        public void OnMouseUp() {
+            if (mouseDown) {
+                OrbPanel.OnEndDrag();
+            }
+            mouseDown = false;
+        }
+
+        public void OnDragDelegate(PointerEventData data) {
+            mouseDown = false;
+            OrbPanel.OnDrag(data);
+        }
+
+        public void OnEndDragDelegate(PointerEventData data) {
+            OrbPanel.OnEndDrag();
+        }
+
+        public void OnSelectedOrb() {
+            GetComponent<SpriteRenderer>().color -= new Color(0, 0, 0, selectedAlpha);
+        }
+
+        public void OnDeselectOrb() {
+            GetComponent<SpriteRenderer>().color += new Color(0, 0, 0, selectedAlpha);
+        }
+
+        public void OnSwap(int newType) {
+            type = newType;
+            GetComponent<SpriteRenderer>().sprite = getCurrentSprite();
+        }
+
+        private void move() {
+            if (_preRotateionPos.magnitude == 0) {
+                _preRotateionPos = transform.position;
+                counter = animationConstant + 1;
+            }
+            transform.position += moveSpeed * Time.deltaTime;
+            counter--;
+            if (counter <= 0) {
+                moveTarget = Vector3.zero;
+                transform.position = _preRotateionPos;
+            }
+        }
+
+
+        private Sprite getCurrentSprite() {
+            if (type == 1) {
+                return typeOneOrb;
+            }
+            else if (type == 2) {
+                return typeTwoOrb;
+            }
+            else {
+                return typeThreeOrb;
+            }
+        }
+
+        private static int GetRandomNumber(int min, int max) {
+            lock (getrandom) // synchronize
+            {
+                return getrandom.Next(min, max);
+            }
+        }
+
+    }
+
+}
---
+++ Assets/Scripts/Orbs/OrbPanel.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace Assets.Scripts.Orbs {
+
+    /// <summary>
+    /// Static class that controls the Orb panel and Orbs
+    /// </summary>
+    public static class OrbPanel {
+
+        private static readonly int trackingZ = 1;
+
+        private static Orb[,] orbs = new Orb[5, 6];
+        private static Orb selectedOrb = null;
+        private static GameObject currentTracker = null;
+        private static int originalSelectedType = -1;
+
+        /// <summary>
+        /// Each Orb instance should register here at Start() method
+        /// </summary>
+        /// <param name="orb">Orb instance</param>
+        /// <param name="row">Row number of the Orb that starts at 0</param>
+        /// <param name="column">Column number of the Orb that starts at 0</param>
+        public static void regOrb(Orb orb, int row, int column) {
+            orbs[row, column] = orb;
+        }
+
+        public static void OnBeginDrag(Orb orb, int row, int column) {
+            spawnTrackingOrb(orb);
+            orb.OnSelectedOrb();
+            selectedOrb = orb;
+            originalSelectedType = orb.getType();
+        }
+
+        public static void OnDrag(PointerEventData ev) {
+            // Update tracker position
+            Vector3 pointerPos = Camera.main.ScreenToWorldPoint(new Vector3(ev.position.x, ev.position.y, 10));
+            pointerPos.z = trackingZ;
+            currentTracker.transform.position = pointerPos;
+            // Check if any orbs has to be swapped
+            GameObject raycastedObj = ev.pointerCurrentRaycast.gameObject;
+            if (raycastedObj != null) {
+                Orb newSelectedOrb = raycastedObj.GetComponent<Orb>();
+                if (newSelectedOrb != null && newSelectedOrb != selectedOrb) {
+                    // Swap the type of the 2 orb
+                    selectedOrb.OnSwap(newSelectedOrb.getType());
+                    newSelectedOrb.OnSwap(selectedOrb.getType());
+                    // Select the new orb
+                    selectedOrb.OnDeselectOrb();
+                    newSelectedOrb.OnSelectedOrb();
+                    selectedOrb = newSelectedOrb;
+                }
+            }
+        }
+
+        public static void OnEndDrag() {
+            selectedOrb.OnSwap(originalSelectedType);
+            selectedOrb.OnDeselectOrb();
+            originalSelectedType = -1;
+            selectedOrb = null;
+            UnityEngine.Object.Destroy(currentTracker);
+            List<List<Orb>> matches = MatchingAlgo.matchOrb(orbs);
+            foreach (List<Orb> lo in matches) {
+                String a = "";
+                foreach (Orb o in lo) {
+                    a += o;
+                }
+                Debug.Log(a);
+            }
+        }
+
+        private static void spawnTrackingOrb(Orb original) {
+            GameObject trackingObj = new GameObject();
+            SpriteRenderer sr = trackingObj.AddComponent<SpriteRenderer>();
+            sr.sprite = original.GetComponent<SpriteRenderer>().sprite;
+            Vector3 originalPos = original.transform.position;
+            originalPos.x += 0.1f;
+            originalPos.y += 0.1f;
+            originalPos.z = trackingZ;
+            trackingObj.transform.position = originalPos;
+            trackingObj.transform.localScale = new Vector3(0.48f, 0.48f, 1);
+            currentTracker = trackingObj;
+        }
+
+    }
+
+}
