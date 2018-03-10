b345d814ba12cb566f3efa8be00644a8e9f12d33
Ken Sze <acken2@outlook.com>
2018-03-11 00:59:13 +0900

    Added script for playing SFX when orb move or get eliminated

---
+++ Assets/Scripts/Orbs/Sound/OrbEliminationSFX.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts.Orbs.Sound {
+
+    /// <summary>
+    /// Sound script that used to play the combo soundtrack
+    /// </summary>
+    [RequireComponent(typeof(AudioSource))]
+    public class OrbEliminationSFX: MonoBehaviour {
+
+        // Timer for self destruct
+        private float selfdestruct = 0;
+        // Maximum lifespan of this instance
+        private float selfdestructLimit = 1;
+
+        /// <summary>
+        /// Set this instacne to not self destruct
+        /// </summary>
+        public void SetEternal() {
+            selfdestructLimit = float.MaxValue;
+        }
+
+        /// <summary>
+        /// Check for self destruction per frame till destructed
+        /// </summary>
+        public void Update() {
+            if (selfdestruct < selfdestructLimit) {
+                selfdestruct += Time.deltaTime;
+            }
+            else {
+                Destroy(this.gameObject);
+            }
+        }
+
+        /// <summary>
+        /// Play the combo sound
+        /// </summary>
+        /// <param name="pitch">Calculated pitch to be played</param>
+        /// <param name="delay">Delay in second before the sound is played</param>
+        public void playSound(float pitch, float delay) {
+            GetComponent<AudioSource>().pitch = pitch;
+            GetComponent<AudioSource>().PlayDelayed(delay);
+            selfdestructLimit += delay; // Prolong this instance for delay seconds
+        }
+
+    }
+
+}
---
+++ Assets/Scripts/Orbs/Sound/SoundSystem.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts.Orbs.Sound {
+
+    public class SoundSystem: MonoBehaviour {
+
+        public static SoundSystem instance;
+
+        public OrbEliminationSFX orbEliminationTemplate;
+        public int startingPitch = 1;
+        public float incrementPitch = 0.1f;
+
+        public AudioSource orbMovement;
+
+        public void Start() {
+            instance = this;
+            orbEliminationTemplate.SetEternal();
+        }
+
+        public void playComboSFX(int combo, float delay) {
+            OrbEliminationSFX oesfx = Instantiate(orbEliminationTemplate);
+            oesfx.playSound(startingPitch + incrementPitch * combo, delay);
+        }
+
+        public void playMovementSFX() {
+            orbMovement.Play();
+        }
+
+    }
+
+}
