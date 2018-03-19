c5f00848c60d21cdd2c5aee321df3320fb786e85
Ken Sze <acken2@outlook.com>
2018-03-19 10:33:33 +0900

    Implemented initialization script for data classes

---
+++ Assets/Scripts/Data/DataInit.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts.Data {
+
+    /// <summary>
+    /// Initialize the Data classes, should be run when the game launch once only
+    /// </summary>
+    public class DataInit : MonoBehaviour {
+
+        /// <summary>
+        /// Initialize data classes and then self destruct
+        /// </summary>
+        public void Start() {
+            Games.LoadGame();
+            Skills.LoadSkill();
+            Destroy(this);
+        }
+
+    }
+
+}
