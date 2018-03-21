cfb09909a5d5f2a791af02042a61ebfea3dce756
Ken Sze <acken2@outlook.com>
2018-03-21 18:04:22 +0900

    Added UI control classes for GameExternal

---
+++ Assets/Scripts/External/Common/Fader.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts.External.Common {
+
+    /// <summary>
+    /// UI class for controlling the Fader in GameExternal
+    /// </summary>
+    public class Fader : MonoBehaviour {
+
+        /// <summary>
+        /// Allow static access
+        /// </summary>
+        public static Fader instance;
+
+        /// <summary>
+        /// Animator of Fader gameobject
+        /// </summary>
+        private Animator animator;
+
+        /// <summary>
+        /// Initialize references
+        /// </summary>
+        public void Start() {
+            instance = this;
+            animator = GetComponent<Animator>();
+        }
+
+        /// <summary>
+        /// Begin fade out animation of the GameExternal scene
+        /// </summary>
+        public void FadeOut() {
+            animator.SetBool("fadeOut", true);
+        }
+
+    }
+
+}
---
+++ Assets/Scripts/External/GameSelection/LoadButton.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Assets.Scripts.External.GameSelection {
+
+    /// <summary>
+    /// UI class for controlling the game selection button
+    /// </summary>
+    public class LoadButton : MonoBehaviour {
+
+        /// <summary>
+        /// Game index to be loaded when this button is clicked
+        /// </summary>
+        public int gameIndex = 0;
+
+        /// <summary>
+        /// Function triggered when the button is clicked
+        /// </summary>
+        public void OnClick() {
+            // Set game index to be loaded
+            Orbs.Coordinator.StageManager.gameIndex = gameIndex;
+            // Fade the scene out
+            Common.Fader.instance.FadeOut();
+            // Moved to the next game core scene, and ask it to load as soon as possible
+            SceneManager.LoadSceneAsync("GameCore").allowSceneActivation = true;
+        }
+
+    }
+
+}
