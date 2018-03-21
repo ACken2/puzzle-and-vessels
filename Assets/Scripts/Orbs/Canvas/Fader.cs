3a889b2e381bdc0f8a63b385052e471827e5d748
Ken Sze <acken2@outlook.com>
2018-03-21 16:00:17 +0900

    Implemented UI class for Fader

---
+++ Assets/Scripts/Orbs/Canvas/Fader.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts.Orbs.Canvas {
+
+    public class Fader : MonoBehaviour {
+
+        /// <summary>
+        /// Allow static access
+        /// </summary>
+        public static Fader instance;
+
+        /// <summary>
+        /// Animator of Fader gameobject
+        /// </summary>
+        private Animator animator;
+        
+        /// <summary>
+        /// Initialize references
+        /// </summary>
+        public void Start() {
+            instance = this;
+            animator = GetComponent<Animator>();
+        }
+
+        /// <summary>
+        /// Begin fade out animation of the scene, called by Coordinator to prepare for scene change
+        /// </summary>
+        public void FadeOut() {
+            animator.SetBool("fadeOut", true);
+        }
+
+        /// <summary>
+        /// Triggered when fade out animation is completed
+        /// </summary>
+        public void OnFadeOutDone() {
+            Coordinator.Coordinator.NotifyFadedOut(); // Notify Coordinator
+        }
+
+    }
+
+}
