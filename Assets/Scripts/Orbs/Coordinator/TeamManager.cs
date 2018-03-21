4bd9e3573074a472d19761a3f46944245ed63e4d
Ken Sze <acken2@outlook.com>
2018-03-21 15:24:56 +0900

    Added a preliminary TeamManager class that returns a static member array

---
+++ Assets/Scripts/Orbs/Coordinator/TeamManager.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections.Generic;
+
+namespace Assets.Scripts.Orbs.Coordinator {
+
+    /// <summary>
+    /// Static class responsible for tracking the current team member selected
+    /// </summary>
+    public static class TeamManager {
+
+        /// <summary>
+        /// Array storing the UUID of the member selected
+        /// </summary>
+        private static List<string> members = new List<string>();
+
+        /// <summary>
+        /// Add a new member to the selection
+        /// </summary>
+        /// <param name="member">UUID of the new team member</param>
+        /// <returns>Boolean stating whether the addition is successful or not</returns>
+        public static bool AddMember(string member) {
+            if (members.Count >= 6) {
+                // Reject if there is already 6 chosen member
+                return false;
+            }
+            else {
+                // Accept addition request
+                members.Add(member);
+                return true;
+            }
+        }
+
+        /// <summary>
+        /// Remove a member from the selection
+        /// </summary>
+        /// <param name="member">Member to be removed</param>
+        public static void RemoveMember(string member) {
+            members.Remove(member);
+        }
+
+        /// <summary>
+        /// Return the current list of selected member
+        /// </summary>
+        /// <returns>List of string that detail the currently selected member</returns>
+        public static List<string> GetMembers() {
+            List<string> members = new List<string>();
+            members.Add("ce42489e-2b75-472e-be02-5a8e4b4747d5");
+            return members;
+            //return members;
+        }
+
+    }
+
+}
